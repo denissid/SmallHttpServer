9ca659ce4404c2c6cdce108ba6a654d61debd991
Denis <densid@mail.ru>
2016-02-25 16:07:42 +0300

    Added path

--- Logger.h
+++ Logger.h
@@ -7,6 +7,6 @@ static void WriteLog(const std::string& message)
 {
 	using namespace std;
 
-	static fstream file ("log.txt", std::fstream::in | std::fstream::out|std::fstream::app);
+	static fstream file ("/home/box/log.txt", std::fstream::in | std::fstream::out|std::fstream::app);
 	file << message << endl;
 }
